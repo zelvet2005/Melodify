2b9633c8a8a66ba3ba3f429a97a82dec2d9843e2
Vetal <vitalikzeleneckij3@gmail.com>
2025-01-27 20:02:47 +0200

    implemented play-pause, next and previous songs functions

--- src/ButtonsPanel/ButtonsPanel.jsx
+++ src/ButtonsPanel/ButtonsPanel.jsx
@@ -1,23 +1,83 @@
 import "./ButtonsPanel.css";
+import PropTypes from "prop-types";
 
-export default function ButtonsPanel() {
+export default function ButtonsPanel({
+  audio,
+  timer,
+  isPlay,
+  setIsPlay,
+  setTimer,
+  setCurrentTime,
+  nextSong,
+  previousSong,
+}) {
+  function handlePlay() {
+    const newIsPlaying = !isPlay;
+    setIsPlay(newIsPlaying);
+    if (newIsPlaying) {
+      playAudio();
+    } else {
+      stopAudio();
+    }
+  }
+  function playAudio() {
+    audio.current.play();
+    setTimer(
+      setInterval(() => {
+        setCurrentTime((prev) => prev + 1);
+      }, 1000)
+    );
+  }
+  function stopAudio() {
+    audio.current.pause();
+    clearInterval(timer);
+    setTimer(null);
+  }
+
   return (
     <div role="buttons-panel" className="buttons-panel">
-      <button>
-        <img src="/images/previous.svg" alt="previous" />
+      <button
+        onClick={() => {
+          previousSong();
+          setIsPlay(false);
+          stopAudio();
+        }}
+      >
+        <img src="/images/previous.svg" alt="previous" draggable={false} />
       </button>
       <button>
-        <img src="/images/random.svg" alt="random" />
+        <img src="/images/random.svg" alt="random" draggable={false} />
+      </button>
+      <button onClick={handlePlay}>
+        <img
+          src={isPlay ? "/images/stop.svg" : "/images/play.svg"}
+          alt="play or stop"
+          draggable={false}
+        />
       </button>
       <button>
-        <img src="/images/play.svg" alt="play or stop" />
+        <img src="/images/repeat.svg" alt="repeat" draggable={false} />
       </button>
-      <button>
-        <img src="/images/repeat.svg" alt="repeat" />
-      </button>
-      <button>
-        <img src="/images/next.svg" alt="next" />
+      <button
+        onClick={() => {
+          nextSong();
+          setIsPlay(false);
+          stopAudio();
+        }}
+      >
+        <img src="/images/next.svg" alt="next" draggable={false} />
       </button>
     </div>
   );
 }
+
+ButtonsPanel.propTypes = {
+  audio: PropTypes.object.isRequired,
+  timer: PropTypes.number.isRequired,
+  isPlay: PropTypes.bool.isRequired,
+  setIsPlay: PropTypes.func.isRequired,
+  setTimer: PropTypes.func.isRequired,
+  setCurrentTime: PropTypes.func.isRequired,
+  nextSong: PropTypes.func.isRequired,
+  previousSong: PropTypes.func.isRequired,
+};
--- src/Player/Player.jsx
+++ src/Player/Player.jsx
@@ -1,22 +1,92 @@
 import { useLocation, useParams } from "react-router-dom";
 import "./Player.css";
 import ButtonsPanel from "../ButtonsPanel/ButtonsPanel";
+import { useEffect, useRef, useState } from "react";
 
 export default function Player() {
   const location = useLocation();
   const musics = location.state;
   const { name } = useParams();
+  const audioElement = useRef();
+
+  const [currentMusicIndex, setCurrentMusicIndex] = useState(
+    musics.findIndex((music) => music.name === name)
+  );
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(-1);
+  const [timer, setTimer] = useState(null);
+  const [isPlay, setIsPlay] = useState(false);
+
+  useEffect(() => {
+    if (currentTime === Math.floor(duration)) {
+      nextSong();
+      clearInterval(timer);
+      setTimer(null);
+      setIsPlay(false);
+    }
+  }, [currentTime]);
+
+  function handleRewind(e) {
+    const newTime = e.target.value;
+    setCurrentTime(+newTime);
+    audioElement.current.currentTime = newTime;
+  }
+  function nextSong() {
+    setCurrentMusicIndex(
+      currentMusicIndex === musics.length - 1 ? 0 : currentMusicIndex + 1
+    );
+    setCurrentTime(0);
+  }
+  function previousSong() {
+    setCurrentMusicIndex(
+      currentMusicIndex === 0 ? musics.length - 1 : currentMusicIndex - 1
+    );
+    setCurrentTime(0);
+  }
+  function getFormattedTime(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes > 9 ? minutes : `0${minutes}`}:${
+      seconds > 9 ? seconds : `0${seconds}`
+    }`;
+  }
 
   return (
     <section className="player-container">
-      <img src="/images/record.svg" alt="" className="record-img" />
-      <h3 className="song-name">{name}</h3>
-      <input type="range" className="range" />
+      <audio
+        src={musics[currentMusicIndex].src}
+        ref={audioElement}
+        onLoadedMetadata={() => setDuration(audioElement.current.duration)}
+      ></audio>
+      <img
+        src="/images/record.svg"
+        alt=""
+        className="record-img"
+        draggable={false}
+      />
+      <h3 className="song-name">{musics[currentMusicIndex].name}</h3>
+      <input
+        type="range"
+        className="range"
+        min={0}
+        max={duration}
+        value={currentTime}
+        onChange={(e) => handleRewind(e)}
+      />
       <div role="time-block" className="time-block">
-        <time>00:00</time>
-        <time>03:23</time>
+        <time>{getFormattedTime(currentTime)}</time>
+        <time>{getFormattedTime(duration)}</time>
       </div>
-      <ButtonsPanel />
+      <ButtonsPanel
+        audio={audioElement}
+        timer={timer}
+        isPlay={isPlay}
+        setIsPlay={setIsPlay}
+        setTimer={setTimer}
+        setCurrentTime={setCurrentTime}
+        nextSong={nextSong}
+        previousSong={previousSong}
+      />
     </section>
   );
 }
